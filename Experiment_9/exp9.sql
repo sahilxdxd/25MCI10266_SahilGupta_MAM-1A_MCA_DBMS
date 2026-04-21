b9d75f2e210fe16dbb79bee3c7bfb49258607e55
Sahil Gupta <guptasahil6072@gmail.com>
2026-04-21 13:05:59 +0530

    Add files via upload

---
+++ Experiment_9/exp9.sql
@@ -0,0 +1,38 @@
+CREATE TABLE employee (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    working_hours INT,
+    perhour_salary NUMERIC,
+    total_payable_amount NUMERIC
+);
+Select * from  employee
+
+CREATE OR REPLACE FUNCTION CACULATE_PAYABLE_AMOUNT() RETURNS TRIGGER
+AS
+$$
+  BEGIN
+		NEW.total_payable_amount:=NEW.perhour_salary*New.working_hours;
+
+		IF NEW.total_payable_amount>25000 THEN
+		RAISE EXCEPTION 'INVALID ENTRY BEACUSE PAYABLE AMOUNT CAN NOT BE GREATER THAN 25000';
+		END IF;
+
+		RETURN NEW;
+   END;
+
+$$ LANGUAGE PLPGSQL;
+
+
+CREATE OR REPLACE TRIGGER AUTOMATED_PAYABLE_AMOUNT_CALCULATION
+BEFORE INSERT
+ON  employee
+FOR EACH ROW
+EXECUTE FUNCTION CACULATE_PAYABLE_AMOUNT()
+
+	
+INSERT INTO EMPLOYEE(EMP_ID, EMP_NAME,working_hours,perhour_salary)
+VALUES (1, 'AKASH',10,1000);
+INSERT INTO EMPLOYEE(EMP_ID, EMP_NAME,working_hours,perhour_salary)
+VALUES (1, 'AKASH',10,100000);
+
+SELECT * FROM EMPLOYEE;
